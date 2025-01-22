d6376b020821dfd93a522076c687e492d2a86fbb
dmonarchc <cisternasdelriodaniel@gmail.com>
2025-01-21 21:50:40 -0300

    Final part of the course

--- OOP_CPP/object_oriented.cpp
+++ OOP_CPP/object_oriented.cpp
@@ -46,6 +46,9 @@ public:
     else
       std::cout << Name << ", sorry NO promotion for you!" << std::endl;
   }
+  virtual void Work() {
+    std::cout << Name << " is checking email, task backlog, performing task..." << std::endl;
+  }
 };
 
 class Developer:public Employee {
@@ -59,12 +62,49 @@ public:
   void FixBug() {
     std::cout << Name << " fixed bug using " << FavProgrammingLanguage << std::endl;
   }
+  void Work() {
+    std::cout << Name << " is writing " << FavProgrammingLanguage << " code " << std::endl;
+  }
 };
+
+class Teacher:public Employee{
+public:
+  string Subject;
+  void PrepareLesson() {
+    std::cout << Name << " is preparing " << Subject << " lesson " << std::endl;
+  }
+  Teacher(string name, string company, int age, string subject)
+    :Employee(name, company, age)
+  {
+    Subject = subject;
+  }
+  void Work() {
+    std::cout << Name << " is teaching " << Subject << std::endl;
+  }
+};
+
 int main()
 {
   Employee employee1("Daniel", "dmonach", 25);
   Employee employee2("John", "Amazon", 35);
+
+  //Inheritance
+  std::cout << "Inheritance" << std::endl;
   Developer d = Developer("Sardina", "Lore", 23, "HTML");
   d.FixBug();
   d.AskForPromotion();
+  Teacher t = Teacher("Jack", "Cool School", 35, "History");
+  t.PrepareLesson();
+  t.AskForPromotion();
+  d.Work();
+  t.Work();
+  std::cout << "\n" << std::endl;
+
+  // Polymorphism
+  std::cout << "Polymorphism" << std::endl;
+  Employee* e1 = &d;
+  Employee* e2 = &t;
+
+  e1->Work();
+  e2->Work();
 }
